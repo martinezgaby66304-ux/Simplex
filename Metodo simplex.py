2e78c1091d26b967352e5e764259741325cc3220
martinezgaby66304 <martinezgaby66304@gmail.com>
2025-12-02 17:48:01 -0600

    Tipo de funcion maxi, mini

--- Metodo simplex.py
+++ Metodo simplex.py
@@ -1,6 +1,22 @@
 class Simplex:
-    def __init__(self, funcion, restriccion, ):
+    def __init__(self, funcion, restriccion, tipo):
         self.funcion = funcion
         self.restriccion = restriccion
+        self.tipo = tipo
 
-   
+def Mostrar_tipo(tipo):
+    if tipo == 1:
+        return "Maximización"
+    elif tipo == 2:
+        return "Minimización"
+    
+def convertir_a_maximizacion_minimizacion(tipo, funcion, restriccion):
+    funcion_convertida = []
+    if tipo == 1:
+        for coeficiente in funcion:
+            funcion_convertida.append(coeficiente)
+    elif tipo == 2:
+        for coeficiente in funcion:
+            funcion_convertida.append(-coeficiente)
+            
+    return funcion_convertida, restriccion
