fd89b204af06f440bd763e7daaf4a7e258b2f2ff
martinezgaby66304 <martinezgaby66304@gmail.com>
2025-12-03 18:53:23 -0600

    Recibe datos, agregando limites y restricciones

--- Metodo simplex.py
+++ Metodo simplex.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Simplex:
     def __init__(self, funcion, restriccion, tipo):
         self.funcion = funcion
@@ -20,3 +22,20 @@ def convertir_a_maximizacion_minimizacion(tipo, funcion, restriccion):
             funcion_convertida.append(-coeficiente)
             
     return funcion_convertida, restriccion
+
+print("¿Qué tipo de problema quieres resolver?")
+print("1 = Maximización")
+print("2 = Minimización")
+tipo = int(input("Ingrese el tipo de problema: "))
+
+funcion = [float(x) for x in input("Ingrese los coeficientes de la función objetivo separados por espacios: ").split()]
+
+restricciones = []
+limites = []
+n = int(input("Ingrese el número de restricciones: "))
+
+for i in range(n):
+    coeficientes = [float(x) for x in input(f"Ingrese los coeficientes de la restricción {i+1} separados por espacios: ").split()]
+    restricciones.append(coeficientes)
+    limite = float(input(f"Ingrese el límite de la restricción {i+1}: "))
+    limites.append(limite)
